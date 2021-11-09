75e623e5a82ee83359056c0d649f4dad6ccc067e
Gabby Recny <gjtrecny@gmail.com>
2021-11-08 20:27:29 -0600

    Adjust code

--- main.js
+++ main.js
@@ -5,7 +5,6 @@ var studyButton = document.querySelector(".study");
 var meditateIcon = document.getElementById("meditateIcon");
 var exerciseIcon = document.getElementById("exerciseIcon");
 var studyIcon = document.getElementById("studyIcon");
-// var activityButtons = document.querySelector("activity-buttons");
 var timeInput = document.querySelector(".time");
 var activityButton = document.querySelector(".start-activity");
 var accomplishInput = document.querySelector(".accomp-input");
@@ -20,12 +19,11 @@ var minError = document.getElementById("minutesError");
 var secError = document.getElementById("secondsError");
 
 
-
 activityButton.addEventListener("click", checkInputField);
 categoryButtons.addEventListener("click", selectButton)
-// meditateButton.addEventListener("click", activateMeditate);
-// exerciseButton.addEventListener("click", activateExercise);
-// studyButton.addEventListener("click", activateStudy);
+meditateButton.addEventListener("click", activateMeditate);
+exerciseButton.addEventListener("click", activateExercise);
+studyButton.addEventListener("click", activateStudy);
 timeInput.addEventListener("keydown", function() {
   preventNumInputs(event)
 });
@@ -34,50 +32,50 @@ timeInput.addEventListener("keydown", function() {
 function selectButton() {
   console.log(categoryButtons.id)
 }
-//
-// function activateMeditate() {
-//   meditateButton.classList.add('activate-meditate');
-//   meditateIcon.setAttribute('src', 'assets/meditate-active.svg');
-//   meditateIcon.setAttribute('alt', 'active meditate');
-//
-//   removeStudyClass()
-//   removeExerciseClass()
-// }
-//
-// function activateExercise() {
-//   exerciseButton.classList.add('activate-exercise')
-//   exerciseIcon.setAttribute('src', 'assets/exercise-active.svg');
-//   exerciseIcon.setAttribute('alt', 'active exercise');
-//
-//   removeStudyClass()
-//   removeMeditateClass()
-// }
-//
-// function activateStudy() {
-//   studyButton.classList.add('activate-study')
-//   studyIcon.setAttribute('src', 'assets/study-active.svg');
-//   studyIcon.setAttribute('alt', 'active study');
-//
-//   removeExerciseClass()
-//   removeMeditateClass()
-// }
-//
-// function removeExerciseClass() {
-//   exerciseButton.classList.remove('activate-exercise');
-//   exerciseIcon.setAttribute('src', 'assets/exercise.svg');
-//   exerciseIcon.setAttribute('alt', 'passive exercise');
-// }
-// function removeMeditateClass() {
-//   meditateButton.classList.remove('activate-meditate');
-//   meditateIcon.setAttribute('src', 'assets/meditate.svg');
-//   meditateIcon.setAttribute('alt', 'passive meditate')
-// }
-//
-// function removeStudyClass() {
-//   studyButton.classList.remove('activate-study')
-//   studyIcon.setAttribute('src', 'assets/study.svg');
-//   studyIcon.setAttribute('alt', 'passive study');
-// }
+
+function activateMeditate() {
+  meditateButton.classList.add('activate-meditate');
+  meditateIcon.setAttribute('src', 'assets/meditate-active.svg');
+  meditateIcon.setAttribute('alt', 'active meditate');
+
+  removeStudyClass()
+  removeExerciseClass()
+}
+
+function activateExercise() {
+  exerciseButton.classList.add('activate-exercise')
+  exerciseIcon.setAttribute('src', 'assets/exercise-active.svg');
+  exerciseIcon.setAttribute('alt', 'active exercise');
+
+  removeStudyClass()
+  removeMeditateClass()
+}
+
+function activateStudy() {
+  studyButton.classList.add('activate-study')
+  studyIcon.setAttribute('src', 'assets/study-active.svg');
+  studyIcon.setAttribute('alt', 'active study');
+
+  removeExerciseClass()
+  removeMeditateClass()
+}
+
+function removeExerciseClass() {
+  exerciseButton.classList.remove('activate-exercise');
+  exerciseIcon.setAttribute('src', 'assets/exercise.svg');
+  exerciseIcon.setAttribute('alt', 'passive exercise');
+}
+function removeMeditateClass() {
+  meditateButton.classList.remove('activate-meditate');
+  meditateIcon.setAttribute('src', 'assets/meditate.svg');
+  meditateIcon.setAttribute('alt', 'passive meditate')
+}
+
+function removeStudyClass() {
+  studyButton.classList.remove('activate-study')
+  studyIcon.setAttribute('src', 'assets/study.svg');
+  studyIcon.setAttribute('alt', 'passive study');
+}
 
 function preventNumInputs(event) {
 var invalidChars = [
@@ -94,7 +92,7 @@ var invalidChars = [
 
 function checkInputField() {
   for (var i = 0; i < errorMessage.length; i++) {
-    if (!errorMessage[i].value || !categoryButtons.checked) {
+    if (!errorMessage[i].value || !categoryButtons.id) {
       displayErrorMessages(event)
     } else {
       startActivity()
@@ -134,46 +132,3 @@ function startActivity() {
   var saveActivity = JSON.stringify(freshActivity);
   localStorage.setItem("activity", saveActivity);
 }
-
-
-//Goal: Show error message when user does not select necessary elements
-//Input: Lack there of
-//Output: Error message with customized elements
-
-//Access the empty inputs
-//Check to see if they have values
-//If not, pop up the error messages
-
-//Access the error messages
-//
-
-
-function changeBorderColor() {
-  if(categoryButtons.studyId === "study") {
-    timer.classList.add("activate-study")
-  }
-}
-
-//A Start Activity button is provided to submit the data entered into the form. When the button is clicked, update your data model with an instance of the Activity class
-
-//create start activity button
-//connect it to the values in the form
-//create instance of activity class using those values
-//add to local storage
-
-
-
-//prevent user from submitting a form without filling out the three inputs
-
-//add the error messages inside the html with a hidden class
-//if button is not clicked then error message
-//if the input for accomplish is empty then error message
-//duplicate for minutes and seconds
-
-//then remove those error messages with some logic
-//we want event listeners
-//
-
-
-//JS validation checks
-//.check  .require
